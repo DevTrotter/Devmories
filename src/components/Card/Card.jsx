5cdb67ce59e45c05cdc1669f28f74dde05120b4e
Devtrotter <sebastienchapuy19@gmail.com>
2023-11-22 17:25:32 +0100

    add algo to click and flip card + counter of try

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,31 @@
 import StyledCard from './StyledCard'
+import backCard from '../../assets/backCard.png'
+import { useEffect, useState } from 'react'
 
-export const Card = ({card}) => {
+export const Card = ({card, setSelectedCards, selectedCards}) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  useEffect(() => {
+    if (selectedCards.length === 2 && selectedCards[0] !== selectedCards[1] && selectedCards.includes(card)) {
+          setTimeout(() => {
+            setIsFlipped(false)
+          }, 1000)
+    }
+  }, [selectedCards, card])
+
+  const handleClick = () => {
+    if (selectedCards.length < 2) {
+      setIsFlipped(true)
+      setSelectedCards([...selectedCards, card])
+    }
+  }
   return (
-    <StyledCard>
-        {card}
+    <StyledCard onClick={handleClick}>
+        {
+          isFlipped ?
+          <h1>{card}</h1> :
+          <img src={backCard} alt="back of card" />
+        }
     </StyledCard>
   )
 }
--- src/components/GameArea/StyledGameArea.js
+++ src/components/GameArea/StyledGameArea.js
@@ -4,9 +4,10 @@ const StyledGameArea = styled.section`
     width: 100%;
     height: 100%;
     display: grid;
-    grid-template-columns: auto auto auto auto auto auto;
+    grid-template-columns: auto auto auto;
     align-items: center;
     justify-content: center;
+    padding-top: 106px;
 `
 
 export default StyledGameArea
